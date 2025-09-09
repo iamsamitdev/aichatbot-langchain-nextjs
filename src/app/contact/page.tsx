620f7337f06cba6b6c057c44ccf59007d27d756d
Samit Koyom <samit90daytalk@hotmail.com>
2025-09-09 23:00:04 +0700

    initial commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+        <>
+            <h1>Contact page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, molestias sapiente? Error, earum sunt corporis voluptates possimus eos enim suscipit expedita vel nulla animi, cupiditate quod soluta, ratione magnam eum.</p>
+        </>
+    )
+}
